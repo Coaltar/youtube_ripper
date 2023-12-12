b24175643deb6c142ca0438b18d5ee53b0775d57
Cassandra Coal <smcphl@gmail.com>
2023-12-12 16:45:13 -0600

    Idk reorganize?

--- youtube_downloader/apps.py
+++ youtube_downloader/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WorkerConfig(AppConfig):
+class YoutubeDownloaderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'worker'
+    name = 'youtube_downloader'
--- youtube_scraper/youtube_scraper/urls.py
+++ youtube_scraper/youtube_scraper/urls.py
@@ -1,4 +1,4 @@
-"""ripper_backend URL Configuration
+"""youtube_scraper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
