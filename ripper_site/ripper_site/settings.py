31ba884c2ad61ae44dea5f8bba78c9f4bf8e8413
Cassandra Coal <smcphl@gmail.com>
2023-12-29 00:10:20 -0600

    Fix redis domain in settings.py

--- ripper_site/ripper_site/settings.py
+++ ripper_site/ripper_site/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 # Application definition
 
 INSTALLED_APPS = [
